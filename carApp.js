32f1f8deab8a91d5510ef26113a479100e486ae2
idobarakk <122865696+idobarakk@users.noreply.github.com>
2023-06-09 09:37:30 +0300

    מטלה 4 לקורס

---
+++ carApp.js
@@ -0,0 +1,20 @@
+const btnCheck = document.getElementById('btn-check');
+const btnAdd = document.getElementById('btn-add');
+const btnDelete = document.getElementById('btn-delete');
+const btnExit = document.getElementById('btn-exit');
+
+btnCheck.addEventListener('click', () => {
+	
+});
+
+btnAdd.addEventListener('click', () => {
+	
+});
+
+btnDelete.addEventListener('click', () => {
+	
+});
+
+btnExit.addEventListener('click', () => {
+	
+});
